b119f81a6e562e5b2c8fdba5d3af060f3e2aa823
sa-tony <satouakatsuki@gmail.com>
2019-08-19 18:32:49 -0700

    init

---
+++ flaskr/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+app = Flask(__name__,
+            static_folder = "./dist/static",
+            template_folder = "./dist")
+@app.route('/')
+def index():
+    return render_template("index.html")
